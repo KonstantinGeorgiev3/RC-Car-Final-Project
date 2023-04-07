5b97124d5d205d6d2698e74ab5a7afa8bec5840c
Ivan Iliev <kosio.geogriev03@gmail.com>
2023-04-07 20:34:50 +0300

    Uploading source code and documentation

---
+++ source code/Kosio_Diplomna.ino
@@ -0,0 +1,192 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+// WiFi settings
+#define SSID_AP            "ESP8266_WiFi_Car" // AP server name
+#define PASSWORD_AP        "12345678"         // AP server password
+
+#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
+#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
+#define IN_1  15          // L298N in1 motors Rightx          GPIO15(D8)
+#define IN_2  13          // L298N in2 motors Right           GPIO13(D7)
+#define IN_3  2           // L298N in3 motors Left            GPIO2(D4)
+#define IN_4  0           // L298N in4 motors Left            GPIO0(D3)
+
+int speedCar = 800;         // 400 - 1023.
+int speed_Coeff = 3;
+
+IPAddress local_ip(192, 168, 4, 1);           //IP for AP mode
+IPAddress gateway(192, 168, 100, 1);          //IP for AP mode
+IPAddress subnet(255, 255, 255, 0);           //IP for AP mode
+ESP8266WebServer server(80);
+
+int car_mode = 0;                             // set car drive mode (0 = stop)
+
+// initialize
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("ESP8266 Wifi Car");
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+  car_control(); // stop the car
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  WiFi.softAP(SSID_AP, PASSWORD_AP);
+
+  // setup web server to handle specific HTTP requests
+  server.on("/", HTTP_GET, handle_OnConnect);
+  server.on("/forward", HTTP_GET, handle_forward);
+  server.on("/backward", HTTP_GET, handle_backward);
+  server.on("/left", HTTP_GET, handle_left);
+  server.on("/right", HTTP_GET, handle_right);
+  server.on("/stop", HTTP_GET, handle_stop);
+  server.onNotFound(handle_NotFound);
+
+  //start server
+  server.begin();
+  Serial.println("ESP8266 car server started.");
+  digitalWrite(LED_PIN, LOW);
+}
+
+// handle HTTP requests and control car
+void loop() {
+  server.handleClient();
+  car_control();
+}
+
+// HTTP request: on connect
+void handle_OnConnect() {
+  car_mode = 0;
+  Serial.println("Client connected");
+  server.send(200, "text/html", SendHTML());
+}
+
+// HTTP request: stop car
+void handle_stop() {
+  car_mode = 0;
+  Serial.println("Stopped");
+  server.send(200, "text/html", SendHTML());
+}
+
+// HTTP request: go forward
+void handle_forward() {
+  car_mode = 1;
+  Serial.println("Go forward...");
+  server.send(200, "text/html", SendHTML());
+}
+
+// HTTP request: go backward
+void handle_backward() {
+  car_mode = 2;
+  Serial.println("Go backward...");
+  server.send(200, "text/html", SendHTML());
+}
+
+// HTTP request: turn left
+void handle_left() {
+  car_mode = 3;
+  Serial.println("Turn left...");
+  server.send(200, "text/html", SendHTML());
+}
+
+// HTTP request: turn right
+void handle_right() {
+  car_mode = 4;
+  Serial.println("Turn right...");
+  server.send(200, "text/html", SendHTML());
+}
+
+// HTTP request: other
+void handle_NotFound() {
+  car_mode = 0;
+  Serial.println("Page error");
+  server.send(404, "text/plain", "Not found");
+}
+
+// control car movement
+void car_control() {
+  switch (car_mode) {
+    case 0: // stop car
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+      break;
+    case 1: // go forward
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+      break;
+    case 2: // go backward
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+      break;
+    case 3: // turn left
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+      break;
+    case 4: // turn right
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar / speed_Coeff);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+
+  }
+}
+
+// output HTML web page for user
+String SendHTML() {
+  String html = "<!DOCTYPE html>";
+  html += "<html>";
+  html += "<head>";
+  html += "<title>ESP8266 WiFi Car</title>";
+  html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  html += "<style>body {background-color: Moccasin;} h1 {color: SaddleBrown;} h2 {color: Olive;}</style>";
+  html += "</head>";
+  html += "<body>";
+  html += "<div align=\"center\">";
+  html += "<h1>ESP8266 WiFi Car</h1>";
+  html += "<h2>Press \"stop\" after the server has been reset</h2>";
+  html += "<br>\n";
+  html += "<form method=\"GET\">";
+  html += "<input type=\"button\" value=\"Go forward\" onclick=\"window.location.href='/forward'\">";
+  html += "<br><br>\n";
+  html += "<input type=\"button\" value=\"Go backward\" onclick=\"window.location.href='/backward'\">";
+  html += "<br><br>\n";
+  html += "<input type=\"button\" value=\"Turn left\" onclick=\"window.location.href='/left'\">";
+  html += "<br><br>\n";
+  html += "<input type=\"button\" value=\"Turn right\" onclick=\"window.location.href='/right'\">";
+  html += "<br><br>\n";
+  html += "<input type=\"button\" value=\"Car stop\" onclick=\"window.location.href='/stop'\">";
+  html += "</form>\n";
+  html += "</div>\n";
+  html += "</body>\n";
+  html += "</html>\n";
+  return html;
+}
